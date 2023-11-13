bd6e555fe7b195a387e9b614ed637d2f9f22803e
Nio Homy <NIOHOMY@mail.ru>
2023-11-13 09:25:43 +0300

    optimization added

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,22 @@
 #include <iostream>
 
 using namespace std;
-
+GLuint vbo = 0;
+GLuint vao = 0;
+float points[] = {
+       -0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 0.0f,
+        0.5f,  0.5f,  0.0f, 0.4f, 0.0f, 0.7f,
+       -0.5f, -0.5f,  0.0f, 1.0f, 1.0f, 1.0f,
+        0.5f, -0.5f,  0.0f, 0.2f, 0.2f, 0.2f
+};
+GLuint indices[] = {
+        0, 1, 2,
+        1, 3, 2
+};
+GLuint ebo;
+GLuint vs;
+GLuint fs;
+GLuint shader_programme;
 //GLFWwindow  *window;
 
 const char* vertex_shader =
@@ -17,11 +32,9 @@ const char* vertex_shader =
 "out vec3 ourColor;\n" 
 "void main()\n" 
 "{\n"
-"   gl_Position = vec4(aPos, 0.7);\n" 
+"   gl_Position = vec4(aPos, 0.8);\n" 
 "   ourColor = aColor;\n" 
 "}\0"; 
-
-
 
 const char* fragment_shader =
 "#version 330 core\n" 
@@ -29,7 +42,7 @@ const char* fragment_shader =
 "in vec3 ourColor;\n" 
 "void main()\n" 
 "{\n"
-"   FragColor = vec4(ourColor, 0.7f);\n" 
+"   FragColor = vec4(ourColor, 1.0f);\n" 
 "}\n\0"; 
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -54,56 +67,25 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 void drawGradientSquare()
 {
-    GLuint vbo = 0;
-    GLuint vao = 0; 
-
-    float points[] = {
-       -0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 0.0f, 
-        0.5f,  0.5f,  0.0f, 0.4f, 0.0f, 0.7f, 
-       -0.5f, -0.5f,  0.0f, 1.0f, 1.0f, 1.0f, 
-        0.5f, -0.5f,  0.0f, 0.2f, 0.2f, 0.2f  
-    };
-
-    GLuint indices[] = {
-        0, 1, 2, 
-        1, 3, 2  
-    };
-
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    GLuint shader_programme = glCreateProgram();
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
-
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    
-    GLuint ebo;
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glShaderSource(vs, 1, &vertex_shader, NULL);
-    glCompileShader(vs);
-
-    glShaderSource(fs, 1, &fragment_shader, NULL);
-    glCompileShader(fs);
-
-    glAttachShader(shader_programme, fs);
-    glAttachShader(shader_programme, vs);
-    
-    glLinkProgram(shader_programme);
-
     glUseProgram(shader_programme);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBindVertexArray(vao);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+    // bind shader (glUseProgrm)
+    // bind vao vbo
+    // bind ebo
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    // release ebo
+    // release vao vbo
+    // release shader
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    glUseProgram(0);
 
 }
 
@@ -145,6 +127,36 @@ int main(int argc, char** argv)
     }
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    vs = glCreateShader(GL_VERTEX_SHADER);
+    fs = glCreateShader(GL_FRAGMENT_SHADER);
+    shader_programme = glCreateProgram();
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glAttachShader(shader_programme, fs);
+    glAttachShader(shader_programme, vs);
+
+    glLinkProgram(shader_programme);
 
     while (!glfwWindowShouldClose(window)) {
 
